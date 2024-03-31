d5e9d4b02967912e5c769d7341eef59adb348579
MunyinSam <kelvinsam233@gmail.com>
2024-03-31 09:55:16 +0700

    math

---
+++ Munyin/input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int age;
+    char name[25]; // bytes
+
+    printf("What's your name?");
+    //scanf("%s", &name);
+    fgets(name, 25 ,stdin); // can new line char
+    name[strlen(name)-1] = '\0';
+
+    printf("How old are you?");
+    scanf("%d", &age);
+
+    printf("\nYou are %d years old", age);
+    printf("\nYou are %s", name);
+
+    return 0;
+
+}
---
+++ Munyin/math.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+
+    double A = sqrt(9);
+    double B = pow(2,4);
+    int C = round(3.14);
+    int D = ceil(3.14);
+    int E = floor(3.99);
+    double F = fabs(-100); //absolite
+    double G = log(3);
+    // sin cos tan
+    // %lf long float
+
+    printf("\n%lf", F);
+}
