044966251c6a1a1851c1efe0a7f58392530553fe
MunyinSam <kelvinsam233@gmail.com>
2024-03-31 09:30:29 +0700

    left right align

--- Munyin/main.c
+++ Munyin/main.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+
+int main(){
+
+    //left and right align
+    float item1 = 5.34;
+    float item2 = 6.43;
+    float item3 = 100.54;
+
+    printf("Item 1: %-8.2f dollars\n", item1);
+    printf("Item 1: %-8.2f dollars\n", item2);
+    printf("Item 1: %-8.2f dollars\n", item3);
+}
