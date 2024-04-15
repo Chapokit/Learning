64293c35c0b5532b29d85c7020f6cd8d229d28fd
Wan1049 <wanwunep@gmail.com>
2024-04-15 15:22:41 +0700

    circle area circum

--- YtGrsisgay/ninegay.c
+++ YtGrsisgay/ninegay.c
@@ -1,1 +1,17 @@
-#
+#include <stdio.h>
+
+int main(){
+    const double PI = 3.14159;
+    double radius,circumference,area;
+    
+    printf("Input radius of circle : ");
+    scanf("%lf", &radius);
+
+    circumference = 2 * PI * radius;
+    area = PI * radius * radius;
+
+    printf("circumference : %lf\n", circumference);
+    printf("area : %lf",  area);
+
+    return 0;
+}
