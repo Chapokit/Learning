1bf7e4403ddbeca91c78f0582f1ddc45cfa1abc7
MunyinSam <kelvinsam233@gmail.com>
2024-04-19 18:57:22 +0700

    2

--- Munyin/JS/index.js
+++ Munyin/JS/index.js
@@ -3,6 +3,28 @@
 
 //window.alert("This is an alert")
 //window.alert("Nahbro")
-
+/*
 document.getElementById("myH1").textContent = 'Hello';
 document.getElementById("myP").textContent = 'Yo mei yo';
+
+*/
+
+let x;
+x = 100;
+
+let y = 123;
+
+console.log(x);
+console.log(y);
+
+let age = 37;
+let price = 10.99;
+
+console.log(age);
+console.log(price);
+
+console.log(typeof age);
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`)
+    
