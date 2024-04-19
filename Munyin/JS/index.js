5d83d0c9105ff0e339c242c96011423b3b6f2f2c
MunyinSam <kelvinsam233@gmail.com>
2024-04-19 19:03:48 +0700

    var done

--- Munyin/JS/index.js
+++ Munyin/JS/index.js
@@ -1,24 +1,10 @@
-//console.log('Hello');
-//console.log('I like mushrooms');
+``
 
-//window.alert("This is an alert")
-//window.alert("Nahbro")
-/*
-document.getElementById("myH1").textContent = 'Hello';
-document.getElementById("myP").textContent = 'Yo mei yo';
+let fullName = "Munyin SAM"
+let age = 18;
 
+let student = true;
 
-
-let firstName = "Munyin";
-
-console.log(typeof firstName);
-console.log(`Your name is ${firstName}`);
-
-
-
-let online = true;
-
-console.log(typeof online);
-console.log(`Bro is ${online}`);
-
-*/
+document.getElementById("myH1").textContent = `Your name is ${fullName}`;
+document.getElementById("myP1").textContent = `You are ${age}`;
+document.getElementById("myP2").textContent = `Am i pro ${student}`;
