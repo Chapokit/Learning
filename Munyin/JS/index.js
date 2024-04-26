a75a3dc52254d12d282dd549319b4ebcc691ad90
MunyinSam <kelvinsam233@gmail.com>
2024-04-26 17:47:34 +0700

    counting thingy

--- Munyin/JS/index.js
+++ Munyin/JS/index.js
@@ -1,12 +1,20 @@
 // const = a variable that can't be changed
 
-let pi = 3.14565432;
-let radius;
-let circumference;
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel")
+let count = 0;
 
-radius = window.prompt(`Enter the radius of a circle`);
-radius = Number(radius);
-
-circumference = 2 * pi * radius;
-
-console.log(circumference);
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function () {
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function () {
+    count = 0;
+    countLabel.textContent = count;
+}
