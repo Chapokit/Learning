9000dda7bec96716dace4237200be772d076ca02
MunyinSam <kelvinsam233@gmail.com>
2024-05-01 18:24:34 +0700

    tenary operator

--- Munyin/JS/index.js
+++ Munyin/JS/index.js
@@ -1,33 +1,25 @@
-//.checked = property that determines the checked state of an HTML checkbox
+// ternary operator shortcut ? to if{} else{}
+
+/*
+let age = 21;
+
+let message = age >= 18 ? "You're an Adult" : "You're a minor";
+
+console.log(message);
 
 
-const myCheckBox = document.getElementById("myCheckBox");
-const visaBtn = document.getElementById("visaBtn");
-const masterBtn = document.getElementById("masterBtn");
-const paypalBtn = document.getElementById("paypalBtn");
-const mySubmit = document.getElementById("mySubmit");
-const subResult = document.getElementById("subResult");
-const paymentResult = document.getElementById("paymentResult");
+let time = 16;
+let greeting = time < 12 ? "Good morning" : "Good afternoon!";
+console.log(greeting);
 
-mySubmit.onclick = function(){
 
-    if(myCheckBox.checked){
-        subResult.textContent = "You are subscribe"
-    }
-    else{
-        subResult.textContent = "You aren't subscribe"
-    }
 
-    if (visaBtn.checked){
-        paymentResult.textContent = "You are paying with Visa"
-    }
-    else if (masterBtn.checked){
-        paymentResult.textContent = "You are paying with MasterCard"
-    }
-    else if (paypalBtn.checked) {
-        paymentResult.textContent = "You are paying with Paypal"
-    }
-    else{
-        paymentResult.textContent = "Nothing"
-    }
-}
+let isStudent = true;
+let message = isStudent ? "You are a student" : "You are Not a student";
+console.log(message);
+
+*/
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount/100)}`)
