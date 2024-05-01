18ca24aff40b159ab649c1bfefddc46e1a51b99a
MunyinSam <kelvinsam233@gmail.com>
2024-05-01 18:58:08 +0700

    String Medthod

--- Munyin/JS/index.js
+++ Munyin/JS/index.js
@@ -1,31 +1,47 @@
-// Switch
-/*
-let day = 1;
+// String Method
 
-switch(day){
-    case 1:
-        console.log("It is Monday");
-        break;
-    case 2:
-        console.log("It is Tuesday");
-        break;
-    case 3:
-        console.log("It is Wednesday");
-        break;
-    default:
-        console.log(`${day} is not a day`);
+let Username = "Munyin";
+
+/*
+
+console.log(Username.charAt(0))
+console.log(Username.indexOf("n"))
+console.log(Username.lastIndexOf("n"))
+console.log(Username.length)
+
+
+let Name = "     munyin      ";
+Name = Name.trim(); //cut whitespace
+Name = Name.toUpperCase();
+Name = Name.toLowerCase();
+Name = Name.repeat(3);
+
+let result = Name.startsWith(" ");
+let result2 = Name.endsWith(" ");
+
+if(result){
+    console.log("Your username cant begin with white space")
+}
+else{
+    console.log(Name);
+}
+console.log(result);
+console.log(Name);
+splitName = "mun yin";
+
+let result3 = splitName.includes(" ");
+if(result3){
+    console.log("Cant have a split");
 }
 */
 
-let testScore = 92;
-let letterGrade;
+let phoneNumber = "123-455-4214";
 
-switch(true){
-    case testScore >= 90:
-        letterGrade = "A";
-        break;
-    case testScore >= 80:
-        letterGrade = "B";
-        break;
-}
-console.log(letterGrade);
+phoneNumber = phoneNumber.replaceAll("-","");
+
+console.log(phoneNumber);
+
+phoneNumber = phoneNumber.padStart(15, "0"); //until it's 15 char long
+phoneNumber = phoneNumber.padEnd(15, "0");
+console.log(phoneNumber);
+
