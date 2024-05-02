fcd5cd136bdef94278ef9ca991d6d3108045e475
MunyinSam <kelvinsam233@gmail.com>
2024-05-02 18:00:38 +0700

    functions

--- Munyin/JS/index.js
+++ Munyin/JS/index.js
@@ -1,38 +1,27 @@
-// Number guessing game
+// function
 
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+function happyBirthday(username, age){
+    console.log(`hbd ${username} ${age}`);
+}
 
-let attemps = 0;
-let guess;
-let running = true;
+function isEven(number){
 
-while(running){
+    return number % 2 === 0 ? true : false;
+}
 
+function isValidEmial(email){
 
-    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-    guess = Number(guess);
-    
-    if(isNaN(guess)){
-        window.alert("Please enter a valid number");
-    }
-    else if(guess < minNum || guess > maxNum){
-        window.alert("Please enter a valid number");
+    /*
+    if(email.includes("@")){
+        return true;
     }
     else{
-        attemps++;
-        if(guess < answer){
-            window.alert("Too low!");
-        }
-        else if(guess > answer){
-            window.alert("Too high!");
-        }
-        else{
-            window.alert(`Correct!!! ${attemps} attemps`);
-            running = false;  
-        }
-    }
-5
-      
+        return false;
+    }*/
+
+    return email.includes("@") ? true : false;
 }
+
+happyBirthday("Munyin",18);
+console.log(isEven(10));
+console.log(isValidEmial("kelbvdsfs@"))
