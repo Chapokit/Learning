3ac045ab34c0b852b52101ccd8df3dec8c73a69e
MunyinSam <kelvinsam233@gmail.com>
2024-05-02 17:51:10 +0700

    num guessing game

--- Munyin/JS/index.js
+++ Munyin/JS/index.js
@@ -1,13 +1,38 @@
-// for loop
+// Number guessing game
 
-for(let i = 1; i <= 20; i++){
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-    if(i == 13){
-        break;
+let attemps = 0;
+let guess;
+let running = true;
+
+while(running){
+
+
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
+    
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number");
     }
     else{
-        console.log(i);
+        attemps++;
+        if(guess < answer){
+            window.alert("Too low!");
+        }
+        else if(guess > answer){
+            window.alert("Too high!");
+        }
+        else{
+            window.alert(`Correct!!! ${attemps} attemps`);
+            running = false;  
+        }
     }
-    
+5
+      
 }
-console.log("Dunso");
