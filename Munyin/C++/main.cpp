2215df36de7cd9c34ea4d8a50c2622991c23c928
MunyinSam <kelvinsam233@gmail.com>
2024-06-09 20:50:15 +0700

    var

--- Munyin/C++/main.cpp
+++ Munyin/C++/main.cpp
@@ -2,10 +2,28 @@
 
 int main() {
 
-    int x;
-    x = 5;
+    int age = 21;
+    int years = 2023;
+    int days = 7;
 
-    std::cout << x;
+    double price = 10.99;
+    double gpa = 2.5;
+    double temp = 25.1;
+
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    bool isGay = false;
+    bool power = true;
+
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food = "Piewdsxfw";
+    std::string address = "232e efdswed wefwe";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
 
     return 0;
 }
