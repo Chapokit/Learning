77a055c49876d6be5079f73c1d43c8794a19096a
Wan1049 <wanwunep@gmail.com>
2024-06-15 23:48:00 +0700

    dd

---
+++ wan cpp/awjndjawndjand.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int searchArray(int array[], int size, int element);
+
+int main(){
+
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8 , 9, 10};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int index;
+    int myNum;
+
+    std::cout << "Enter element to search for : " << '\n';
+    std::cin >> myNum;
+
+    index = searchArray(numbers, size, myNum);
+
+    if (index != -1)
+    {
+        std::cout << myNum << " is at index " << index + 1;
+    }
+    else
+    {
+        std::cout << myNum << " is't in the array";
+    }
+    
+
+}
+
+int searchArray(int array[], int size, int element)
+{
+    for(int i = 0; i < size; i++)
+    {
+        if (array[i] == element)
+        {
+            return i;
+        }
+        
+    }
+    return -1;
+}
