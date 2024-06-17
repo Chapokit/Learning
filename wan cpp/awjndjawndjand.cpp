8f92c53a0ca423708ede40f3bef6826f6b8dea9a
Wan1049 <wanwunep@gmail.com>
2024-06-17 15:19:57 +0700

    aaaa

--- wan cpp/awjndjawndjand.cpp
+++ wan cpp/awjndjawndjand.cpp
@@ -1,47 +1,34 @@
 #include <iostream>
 
-void sortArray(int array[], int size);
+
 
 int main(){
 
-    
-    int arrays[] = {8, 3, 5, 1, 4, 9, 7, 2, 10, 6};
-    int size = sizeof(arrays) / sizeof(arrays[0]);
-    
-    
+    std::string foods[5];
+    int size = sizeof(foods) / sizeof(foods[0]);
+    std::string temp;
 
-    for (int i : arrays)
+    for (int i = 0; i < size; i++)
     {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
-
-    sortArray(arrays, size);
-    
-    for (int i : arrays)
-    {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
-    
-    
-}
-
-void sortArray(int array[], int size)
-{
-    int temp = 0;
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - 1; j++)
+        std::cout << "Enter the food you like # or 'q' to quit " << i + 1 <<  " : ";
+        std::getline(std::cin, temp);
+        if (temp == "q")
         {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-             
+            break;
         }
-
+        else
+        {
+            foods[i] = temp;
+        }
+        
     }
+    
+    std::cout << "You like the following food: \n";
+
+    for (int j = 0; !foods[j].empty(); j++)
+    {
+        std::cout << foods[j] << std::endl;
+    }
+    
+    
 }
