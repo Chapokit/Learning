143c3b5bd0b6b1112aa1b1ad7d97fd3caefdb877
MunyinSam <kelvinsam233@gmail.com>
2024-07-14 22:02:51 +0700

    ..

--- Munyin/Problems/q22.py
+++ Munyin/Problems/q22.py
@@ -1,21 +1,19 @@
-n = input()
-
+n = 4
 width = 0
 
-def insert_into_center(original, insert):
+def center(original, insert):
     middle_index = len(original) // 2
     return original[:middle_index] + insert + original[middle_index:]
 
+if n % 2 == 0:
+    width = n - 1
+else:
+    width = n
+
 for i in range(n):
+    line = "-" * (width - 1)
 
-    l = []
+    if i == 0:
+        line = center(line, "*")
 
-    if n % 2 == 0:
-        width = n-1
-    else:
-        width = n
-
-    line = []
-    for j in range(width):
-        
-        
+    print(line)  # Output the result
