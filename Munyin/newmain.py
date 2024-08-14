8c44760b589fb42fa1cc5efc62db4109e74feed3
MunyinSam <kelvinsam233@gmail.com>
2024-08-14 09:19:46 +0700

    wrf

---
+++ Munyin/newmain.py
@@ -0,0 +1,5 @@
+n = int(input("Input width? "))
+l = int(input("Input lenght? "))
+a = int((n*l)//100)
+print(f"The landlord has {a} pieces of land available for sale.")
+print(f"He will earn {a*1400} pounds from the sale.")
