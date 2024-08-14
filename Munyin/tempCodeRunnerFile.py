ac1dd48b777f652268cdeffa51fad297f7e1e191
MunyinSam <kelvinsam233@gmail.com>
2024-08-14 11:24:08 +0700

    bro

--- Munyin/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-n = float(input("Input width? "))
-l = float(input("Input length? "))
-a = (n*l)//100
-print(f"The landlord has {a:.0f} pieces of land available for sale.")
-print(f"He will earn {a*1400:.0f} pounds from the sale.")
